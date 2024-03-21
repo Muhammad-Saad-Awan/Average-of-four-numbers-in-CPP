74c490c7f7356bfafc86cf7d8d15ca48daf963e6
Muhammad Saad <148006898+ssaadd-bot@users.noreply.github.com>
2024-03-22 00:29:19 +0500

    Average of Four numbers given by the user

---
+++ Average.cpp
@@ -0,0 +1,11 @@
+ #include <iostream>
+using namespace std;
+int main()
+{
+float num1,num2,num3,num4,avg;	
+cout<<"Enter four digit number"<<endl;
+cin>>num1>>num2>>num3>>num4;
+avg=num1+num2+num3+num4/4;
+cout<<"Average:"<<avg;
+return 0;
+}
